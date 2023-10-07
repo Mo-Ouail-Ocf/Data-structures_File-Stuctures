81ca9df0b1f9bdc7a7941ebd7531befc1a417ce8
Ouail Mohammed Oucherif <ouailouchi@gmail.com>
2023-10-07 19:05:56 +0100

    added the binary file

--- chapter 1/exo6/exo6.c
+++ chapter 1/exo6/exo6.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-const int MAX_VILLE_LENGTH = 21;
-const int DATE_LENGTH = 11;
-const int RECORD_SIZE = (MAX_VILLE_LENGTH + DATE_LENGTH + sizeof(int));
+#define MAX_VILLE_LENGTH 21
+#define DATE_LENGTH  11
 
 struct TemperatureRecord {
     char city[MAX_VILLE_LENGTH];
@@ -12,25 +11,11 @@ struct TemperatureRecord {
 };
 
 int main(){
-    FILE* file = fopen("test.bin","rb+");
+    FILE* file = fopen("FileResult.bin","rb+");
 
     if (file==NULL){
         printf("Failure of opening the file");
         return 1;
     }
 
-    struct TemperatureRecord dataTable[]={
-    {
-        "chelghoum laid","07/10/2023",30
-    },
-    {
-        "chelghoum laid","06/10/2023",25
-    },
-    {
-        "chelghoum laid","05/10/2023",20
-    },
-    {
-        "chelghoum laid","04/10/2023",23
-    }
-    };
 }
