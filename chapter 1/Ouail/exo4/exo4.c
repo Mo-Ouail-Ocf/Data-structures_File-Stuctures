2c55e6c5e96ac4fa43df2aae5154b42937b76330
Ouail Mohammed Oucherif <ouailouchi@gmail.com>
2023-10-07 21:09:36 +0100

    solve exo4

--- chapter 1/Ouail/exo4/exo4.c
+++ chapter 1/Ouail/exo4/exo4.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 //calculer le nb de ligne
 int main(){
-    FILE
+    FILE*f = fopen("fich.txt","r");
+    if (f==NULL){
+        printf("Erreur lors de l'ouverture du fichier ");
+        return 1;
+    }
+    int nb_ligne=0;
+    char c;
+
+    if (getc(f)==EOF){
+        //fichier vide
+        printf("Le fichier est vide , il contient aucune ligne");
+        return 0;
+    }
+    else {
+        nb_ligne++;
+        rewind(f);
+        do {
+            c=getc(f);
+            if (c=='\n')
+                nb_ligne++;
+        }
+        while (c!=EOF);
+        
+    }
+    printf("Le nombre de ligne du fichier est : %d",nb_ligne);
+    return 0;
 }
