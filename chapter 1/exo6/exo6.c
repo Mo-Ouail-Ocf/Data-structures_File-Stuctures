ce693da9d0a53796967d1ad531c0bb292a675966
RedaAdjab <reda.adjab.cs@gmail.com>
2023-10-07 19:06:54 +0100

    Did nothing

--- chapter 1/exo6/exo6.c
+++ chapter 1/exo6/exo6.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-const int MAX_VILLE_LENGTH = 21;
-const int DATE_LENGTH = 11;
-const int RECORD_SIZE = (MAX_VILLE_LENGTH + DATE_LENGTH + sizeof(int));
+#define MAX_VILLE_LENGTH 21
+#define DATE_LENGTH 11 
+#define RECORD_SIZE (MAX_VILLE_LENGTH + DATE_LENGTH + sizeof(int))
 
 struct TemperatureRecord {
     char city[MAX_VILLE_LENGTH];
-    char date[DATE_LENGTH];
+    char date[11];
     int temperature;
 };
 
+/* void createRecord(*FILE file, e) {
+    s
+} */
+
 int main(){
-    FILE* file = fopen("test.bin","rb+");
+    FILE* file = fopen("FileResault.bin","rb+");
 
     if (file==NULL){
         printf("Failure of opening the file");
@@ -22,16 +26,8 @@ int main(){
     struct TemperatureRecord dataTable[]={
     {
         "chelghoum laid","07/10/2023",30
-    },
-    {
-        "chelghoum laid","06/10/2023",25
-    },
-    {
-        "chelghoum laid","05/10/2023",20
-    },
-    {
-        "chelghoum laid","04/10/2023",23
     }
     };
 
+    return 0;
 }
