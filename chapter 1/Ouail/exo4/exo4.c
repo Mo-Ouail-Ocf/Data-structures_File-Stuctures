575daac9e1a5dcdcbdb0bb246c26fba86666ab37
Ouail Mohammed Oucherif <ouailouchi@gmail.com>
2023-10-07 18:30:02 +0100

    added some files

---
+++ chapter 1/Ouail/exo4/exo4.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+//calculer le nb de ligne
+int main(){
+    FILE
+}
---
+++ chapter 1/exo6/exo6.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+struct Record {
+    char city[20];
+    char date[10];
+    int temp;
+};
+
+int main(){
+    FILE*file = fopen("test.bin","rb+");
+
+    if (file==NULL){
+        printf("Failure of opening the file");
+        return 1;
+    }
+
+    struct Record dataTable[]={
+    {
+        "chelghoum laid","07/10/2023",30
+    },
+    {
+        "chelghoum laid","06/10/2023",25
+    },
+    {
+        "chelghoum laid","05/10/2023",20
+    },
+    {
+        "chelghoum laid","04/10/2023",23
+    }
+    };
+
+}
