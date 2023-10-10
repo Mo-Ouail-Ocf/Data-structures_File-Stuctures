34db22fc4d42d196a8e1864b557eaaabb7ac68d8
Ouail Mohammed Oucherif <ouailouchi@gmail.com>
2023-10-10 22:24:53 +0100

    I added the important data structures for the huffmann algorithm

--- chapter 1/Huffmann Algorithm/HuffmannAlgorithm.c
+++ chapter 1/Huffmann Algorithm/HuffmannAlgorithm.c
@@ -2,8 +2,40 @@
 #include <stdbool.h>
 #include <string.h>
 
-//test
-//sui
+// Queue struct node
+struct queueLeafNode {
+    char character;
+};
+
+// Queue_Tree struct node
+struct queueTreeNode {
+    void*leftSon;  
+    void*rightSon;  
+};
+
+// Union attribute
+union unionAttr {
+    struct queueLeafNode queueLeafNode;
+    struct queueTreeNode queueTreeNode;
+}; 
+
+//queue node
+struct queueNode {
+    bool isLeafNode;
+    int ocurence;
+    void*nextQueueNode;
+    union unionAttr unionAttr;
+};
+
+// queueLeafNode ptr
+typedef struct queueLeafNode *queueLeafNodePtr;
+// queueTreeNode ptr
+typedef struct queueTreeNode *queueTreeNodePtr;
+//queue node ptr
+typedef struct queueNode *queueNodePtr;
+// Huffmann tree
+typedef queueLeafNodePtr HuffmannTree;
+
 int main(){
 
 }
